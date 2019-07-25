2db68f3d1d6e9810cc69b533986084807045e651
javierjcf <javierjcf@gmail.com>
2019-07-26 01:48:20 +0200

    C++ básico

---
+++ tripleX.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+
+    // Print welcome messages to the terminal
+    std::cout << "Hello, World!";
+    std::cout <<std::endl;
+    std::cout << "Tiemes que adivinmmar el pu** código, que divertido!";
+
+    // Declare 3 number code
+    const int a = 4;
+    const int b = 3;
+    const int c = 2;
+    /* Print the sum
+       and print the product
+     */
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
+
+    return 0;
+}
