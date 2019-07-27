001cb923c0779bc567fa3ebf296b0f7cf81657ac
javierjcf <javierjcf@gmail.com>
2019-07-27 19:39:22 +0200

    "Sentencias condicionales, comparación números"

--- tripleX.cpp
+++ tripleX.cpp
@@ -4,23 +4,44 @@ int main()
 {
 
     // Print welcome messages to the terminal
-    std::cout << "Hello, World!";
-    std::cout <<std::endl;
-    std::cout << "Tiemes que adivinmmar el pu** código, que divertido!";
+    std::cout << "Hello, Agent X!\n";
+    std::cout << "Enter the correct code to continue! \n\n";
 
     // Declare 3 number code
-    const int a = 4;
-    const int b = 3;
-    const int c = 2;
+    const int CodeA = 4;
+    const int CodeB = 3;
+    const int CodeC = 2;
+
     /* Print the sum
        and print the product
      */
-    const int sum = a + b + c;
-    const int product = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
-    std::cout << std::endl;
-    std::cout << sum << std::endl;
-    std::cout << product << std::endl;
+    std::cout << "+ There are 3 numbers in the code: ";
+    std::cout << "\n+ The codes add-up to: " << CodeSum;
+    std::cout << "\n+ The codes multiply to give: " << CodeProduct;
+
+    std::cout << "\n\n+ Enter the three numbers of a code separated by spaces: \n";
+    // Store player guess
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA  >> GuessB >> GuessC;
+
+    std::cout << "You entered: " << GuessA << GuessB << GuessC;
+
+    const int GuessSum = GuessA + GuessB + GuessC;
+    const int GuessProduct = GuessA * GuessB * GuessC;
+
+    // Check if player guess is correct
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std::cout << "\nYou Win!";
+    }
+    else
+    {
+        std::cout << "\nYou Loose!";
+    }
+    
 
     return 0;
 }
