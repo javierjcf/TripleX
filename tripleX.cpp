fb0733a6c540ce63f1919bedfb893dcce4963102
javierjcf <javierjcf@gmail.com>
2019-07-27 20:41:52 +0200

    "Añadidos niveles de dificultad"

--- tripleX.cpp
+++ tripleX.cpp
@@ -1,65 +1,68 @@
 #include <iostream>
+#include <ctime>
 
-void PrintIntroduction()
+void PrintIntroduction(int Difficulty)
 {
-    std::cout << "Hello, Agent X!\n";
-    std::cout << "Enter the correct code to continue! \n\n";
+    std::cout << "\n\nYou are a secret agent breaking into a level " << Difficulty;
+    std::cout << " secure server room...\nEnter the correct code to continue...\n\n";
 }
 
-
-bool PlayGame()
+bool PlayGame(int Difficulty)
 {
+    PrintIntroduction(Difficulty);
 
-    // Print welcome messages to the terminal
-    PrintIntroduction();
-
-    // Declare 3 number code
-    const int CodeA = 4;
-    const int CodeB = 3;
-    const int CodeC = 2;
-
-    /* Print the sum
-       and print the product
-     */
+    // Generate Code
+    const int CodeA = rand() % Difficulty + Difficulty;
+    const int CodeB = rand() % Difficulty + Difficulty;
+    const int CodeC = rand() % Difficulty + Difficulty;
+    
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
 
-    std::cout << "+ There are 3 numbers in the code: ";
-    std::cout << "\n+ The codes add-up to: " << CodeSum;
-    std::cout << "\n+ The codes multiply to give: " << CodeProduct;
 
-    std::cout << "\n\n+ Enter the three numbers of a code separated by spaces: \n";
+    // Print CodeSum and CodeProduct to the terminal
+    std::cout << "+ There are 3 numbers in the code";
+    std::cout << "\n+ The codes add-up to: " << CodeSum;
+    std::cout << "\n+ The codes multiply to give: " << CodeProduct << std::endl;
+
     // Store player guess
     int GuessA, GuessB, GuessC;
-    std::cin >> GuessA  >> GuessB >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
-    std::cout << "You entered: " << GuessA << GuessB << GuessC;
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
 
-    const int GuessSum = GuessA + GuessB + GuessC;
-    const int GuessProduct = GuessA * GuessB * GuessC;
-
-    // Check if player guess is correct
+    // Check if the players guess is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "\nYou Win!\n\n\n";
+        std::cout << "\n*** Well done agent! You have extracted a file! Keep going! ***";
         return true;
     }
     else
     {
-        std::cout << "\nYou Loose!\n\n\n";
+        std::cout << "\n*** You entered the wrong code! Careful agent! Try again! ***";
         return false;
     }
 }
 
-
 int main()
 {
-    while(true)
+    srand(time(NULL)); // create new random sequence based on time of day
+    
+    int LevelDifficulty = 1;
+    int const MaxDifficulty = 5;
+
+    while (LevelDifficulty <= MaxDifficulty) // Loop game until all levels completed
     {
-        bool bLevelComplete = PlayGame();
+        bool bLevelComplete = PlayGame(LevelDifficulty);
         std::cin.clear(); // Clears any errors
         std::cin.ignore(); // Discards the buffer
-        PlayGame();
+
+        if (bLevelComplete) 
+        {
+            ++LevelDifficulty;
+        }
     }
+    std::cout << "\n*** Great work agent! You got all the files! Now get out of there! ***\n";
     return 0;
 }
